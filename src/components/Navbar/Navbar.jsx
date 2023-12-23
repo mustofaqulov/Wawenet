47a848356d8abe6a393e3ae849d28651617f66c2
Islombek <mustofaqulov079@gmail.com>
2023-12-23 15:59:53 +0500

    test

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import Logo from '../../assets/icons/'
+import classNames from 'classnames';
+import style from './navbar.module.scss';
+
+export function Navbar() {
+  return <div className={classNames(style.navbar)}>
+
+  </div>;
+}
