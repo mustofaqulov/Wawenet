61f60b46d5d6eef4678d7381565c7c8aef630efc
Islombek <mustofaqulov079@gmail.com>
2023-12-24 22:43:12 +0500

    change _base.scss and style.scss

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,42 @@
-import Logo from '../../assets/icons/logo.svg'
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../assets/icons/logo.svg';
 import classNames from 'classnames';
 import style from './navbar.module.scss';
 
 export function Navbar() {
-  return <div className={classNames(style.navbar)}>
-    <div className={classNames(style.logo)}>
-      <Logo />
+  const linksData = [
+    { id: 1, text: 'About-us', link: '/About-us' },
+    { id: 2, text: 'SERVICES', link: '/SERVICES' },
+    { id: 3, text: 'PRICING', link: '/PRICING' },
+    { id: 4, text: 'BLOG', link: '/BLOG' },
+    { id: 5, text: 'CONTACT', link: '/CONTACT' },
+  ];
+  return (
+    <div
+      className={classNames(
+        'container',
+        style.navbar,
+      )}
+    >
+      <div className={classNames(style.logo)}>
+        <Logo />
+      </div>
+      <ul>
+        {/* {
+          linksData.map(
+            ({id, text, link})=>{
+            return(
+              <li key={id}>
+              <NavLink
+              to={link}>
+                {text}
+              <NavLink />
+              </li>
+            )
+          })
+        } */}
+      </ul>
+      <button>SIGN IN</button>
     </div>
-  </div>;
+  );
 }
