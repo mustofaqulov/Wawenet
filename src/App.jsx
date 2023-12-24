c3743e1e74448fb1f272b106e47cb7b7d1db6dd7
Abdusamad Latipov <abdusamadlatipov67@gmail.com>
2023-12-24 15:39:56 +0300

    add input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Input from "./components/Input/Input.jsx";
-import icon from '../src/components/Input/right-arrow.svg'
 import './styles/style.scss'
 export default function App() {
-  return <Input title={'Enter Your Email Address ...'} type={'text'} icon={icon}/>
+  return <Input title={'Enter Your Email Address ...'} type={'text'} icon={}/>
 }
