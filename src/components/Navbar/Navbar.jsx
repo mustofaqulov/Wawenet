59401b87fb4527f37d61d1306df3e5d89735a10d
Islombek <mustofaqulov079@gmail.com>
2023-12-25 14:30:45 +0500

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,19 @@ import style from './navbar.module.scss';
 
 export function Navbar() {
   const linksData = [
-    { id: 1, text: 'About-us', link: '/About-us' },
-    { id: 2, text: 'SERVICES', link: '/SERVICES' },
-    { id: 3, text: 'PRICING', link: '/PRICING' },
-    { id: 4, text: 'BLOG', link: '/BLOG' },
-    { id: 5, text: 'CONTACT', link: '/CONTACT' },
+    {
+      id: 1,
+      text: 'About-us',
+      link: '/About-us',
+    },
+    {
+      id: 2,
+      text: 'Services',
+      link: '/Services',
+    },
+    { id: 3, text: 'Pricing', link: '/Pricing' },
+    { id: 4, text: 'Blog', link: '/Blog' },
+    { id: 5, text: 'Contact', link: '/Contact' },
   ];
   return (
     <div
@@ -22,19 +30,13 @@ export function Navbar() {
         <Logo />
       </div>
       <ul>
-        {/* {
-          linksData.map(
-            ({id, text, link})=>{
-            return(
-              <li key={id}>
-              <NavLink
-              to={link}>
-                {text}
-              <NavLink />
-              </li>
-            )
-          })
-        } */}
+        {linksData.map(({ id, text, link }) => {
+          return (
+            <li key={id}>
+              <NavLink to={link}>{text}</NavLink>
+            </li>
+          );
+        })}
       </ul>
       <button>SIGN IN</button>
     </div>
